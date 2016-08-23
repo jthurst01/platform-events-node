10d8323469f24402c6d299903a4cc480d62d22cd
Jay Hurst <jhurst@salesforce.com>
2016-08-23 16:04:52 -0700

    Update for Platform Events 22

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ var Promise = require("bluebird");
 var nforce = require('nforce');
 var org = require('../lib/connection');
 
+const util = require('util');
+
 /* home page. */
 router.get('/', function(req, res, next) {
 	res.render('index');
@@ -14,17 +16,16 @@ router.get('/', function(req, res, next) {
 /* Creates a new the record */
 router.post('/', function(req, res, next) {
  
-  var pie = nforce.createSObject('Printer_Ink_Event__e');
+  var pie = nforce.createSObject('Printer_Ink_Level__e');
   pie.set('CustomerId__c', req.body.customerId);
   pie.set('Printer_Model__c', req.body.printerModel);
   pie.set('Printer_Ink_Level__c', req.body.printerInkLevel);
 
-  console.log('req = ' + JSON.stringify(req));
-  console.log('res = ' + JSON.stringify(res));
   console.log('req.body.customerId = ' + req.body.customerId);
   console.log('req.body.printerModel = ' + req.body.printerModel);
   console.log('req.body.printerInkLevel = ' + req.body.printerInkLevel);
-  console.log('pie = ' + JSON.stringify(pie));  
+  
+  console.log(util.inspect(pie, { showHidden: true, depth: null }));
   
   org.insert({ sobject: pie })
     .then(
